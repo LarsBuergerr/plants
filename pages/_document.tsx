2f34d313f85ceaf86d1cb9b566b847745230618d
Lars Buerger <lars.buerger@freenet.de>
2025-09-05 14:56:44 +0200

    fix: update apple-touch-icon links to use correct path in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,10 +8,22 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="public/manifest.json" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/ios/180.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/ios/152.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/ios/120.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/ios/76.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="public/ios/180.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="public/ios/152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="public/ios/120.png"
+        />
+        <link rel="apple-touch-icon" sizes="76x76" href="public/ios/76.png" />
       </Head>
       <body
         className={clsx(
