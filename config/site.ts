6fde2856e28aee1b3c2c29c12215d4ef80bbb319
Lars Buerger <lars.buerger@freenet.de>
2025-09-05 14:42:42 +0200

    fix: update site configuration and enhance document head with manifest and icons

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Plants",
+  description: "Collect and manage your plants :3",
   navMenuItems: [
     {
       label: "Dashboard",
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,17 @@ import { fontSans } from "@/config/fonts";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="public/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/ios/180.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/ios/152.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/ios/120.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/ios/76.png" />
+      </Head>
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <Main />
